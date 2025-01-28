de3d09c67a90096fb4e2f4e57ccd23512f433e4f
hyuta sasaki <s23C1065UR@s.chibakoudai.jp>
2025-01-29 00:33:10 +0900

    launch

---
+++ test/test.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2025 hyuta sasaki
+# SPDX-License-Identifier: BSD-3-Clause
+
+res=0
+
+dir=~
+[ "$1" != "" ] && dir="$1"
+
+sudo apt -y install python3-pip
+pip3 install requests
+
+cd $dir/ros2_ws
+colcon build
+source $dir/.bashrc
+source install/setup.bash && source install/local_setup.bash
+
+timeout 20 ros2 launch mypkg weather_chiba.launch.py > /tmp/weather_data.log
+
+cat /tmp/weather_data.log |
+grep 'Listen: 10'
