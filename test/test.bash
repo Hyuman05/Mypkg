58fe69f58af0dc2424cc7957879279ec30356311
hyuta sasaki <s23C1065UR@s.chibakoudai.jp>
2025-01-29 02:16:56 +0900

    launch

--- test/test.bash
+++ test/test.bash
@@ -16,10 +16,10 @@ colcon build
 source $dir/.bashrc
 source install/setup.bash && source install/local_setup.bash
 
-timeout 200 ros2 launch mypkg weather_chiba.launch.py &> /tmp/test.log
+timeout 200 ros2 launch mypkg weather_chiba.launch.py &> /tmp/weather_data.log
 
 sleep 20
 echo TESTLOG
-cat /tmp/test.log
+cat /tmp/weather_data.log
 
 exit $res
