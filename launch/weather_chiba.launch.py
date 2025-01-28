a272ba44305a925075e9f7bcf3d6235b5b5cd8f5
hyuta sasaki <s23C1065UR@s.chibakoudai.jp>
2025-01-29 00:16:06 +0900

    launch

---
+++ launch/weather_chiba.launch.py
@@ -0,0 +1,19 @@
+import launch
+import launch.actions
+import launch.substitutions
+import launch_ros.actions
+
+
+def generate_launch_description():
+
+    weather_chiba = launch_ros.actions.Node(
+        package = 'mypkg',
+        executable = 'weather_chiba',
+        )
+    listener = launch_ros.actions.Node(
+        package = 'mypkg',
+        executable = 'listener',
+        output = 'screen'
+        )
+
+    return launch.LaunchDescription([weather_chiba, listener])
